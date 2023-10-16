5cf3f71e0a948a9b6f69fa0c4736da574c1022d7
pantelishatz <xapantelis@hotmail.com>
2023-10-16 19:23:55 +0300

    data

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,4 +1,6 @@
 global using BlazorEcommerce.Shared;
+global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 namespace BlazorEcommerce
@@ -10,11 +12,19 @@ namespace BlazorEcommerce
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            app.UseSwaggerUI();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -27,6 +37,8 @@ namespace BlazorEcommerce
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
 
             app.UseHttpsRedirection();
 
