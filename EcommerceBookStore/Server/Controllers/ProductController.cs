03d0b6b506c44a4dfe18c4b6ed390ac550c7b583
pantelishatz <xapantelis@hotmail.com>
2023-10-22 21:17:11 +0300

    Search Products

--- EcommerceBookStore/Server/Controllers/ProductController.cs
+++ EcommerceBookStore/Server/Controllers/ProductController.cs
@@ -38,5 +38,21 @@ namespace EcommerceBookStore.Server.Controllers
 
             return Ok(result);
         }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts(string searchText)
+        {
+            var result = await _productService.SearchProducts(searchText);
+
+            return Ok(result);
+        }
+
+        [HttpGet("searchsuggestions/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSearchSuggestions(searchText);
+
+            return Ok(result);
+        }
     }
 }
